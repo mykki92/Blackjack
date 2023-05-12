c530afc873ee0ab9b195b011af1fe3be29f606c3
mykki92 <>
2023-05-12 19:16:46 +0000

    Create functions to validate and log in existing users and signup new users

--- gsheet.py
+++ gsheet.py
@@ -31,3 +31,40 @@ def update_login_data(data):
     password data input by the user
     """
     USERS.append_row(data)
+
+
+def validate_user_login(user, password):
+    """
+    Function to check if the provided username already exists, and
+    that the username and password entered are valid. If the input
+    provided is invalid an error will be printed to the user
+    """
+    try:
+        if len(user) < 6 or len(password) < 6:
+            raise ValueError(
+                "Username and Password should be at least 6 characters"
+            )
+    except ValueError as v:
+        print(f"\nInvalid Input: {v}")
+        return False
+    try:
+        existing_user = get_login_data()
+        for ind in existing_user:
+            if ind["USERNAME"] == user:
+                raise ValueError(
+                    "\nUsername already exist"
+                )
+    except ValueError as e:
+        print(f"\nInvalid Username: {e}")
+        return False
+    try:
+        if not (isinstance(user, str) or isinstance(password, str)):
+            raise TypeError(
+                "\nEnter details using letters only"
+            )
+    except TypeError as m:
+        print(f"Invalid user input: {m}")
+        return False
+    else:
+        print("\nSign Up confirmed....")
+        return True
--- run.py
+++ run.py
@@ -1,4 +1,90 @@
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Python imports
+import random
+import sys
+import time
+import os
+
 # Internal imports
-from gsheet import login_data
+from gsheet import get_login_data
 from gsheet import update_login_data
+from gsheet import validate_user_login
+
+
+def check_existing_user():
+    """
+    Checks if user already has a login, if yes asks them to log in,
+    if no asks them to sign up
+    """
+    exist_check = input("Have you played before? Y/N\n")
+    if exist_check.upper() == "Y":
+        check_user()
+    elif exist_check.upper() == "N":
+        add_new_user()
+    else:
+        print("Invalid input, type Y or N")
+        time.sleep(1)
+        check_existing_user()
+
+
+def add_new_user():
+    """
+    Validates new login data provided by the user and updates the
+    googlesheet with new details
+    """
+    time.sleep(1)
+    print("\nSIGN UP HERE!!")
+    time.sleep(1)
+    print("\nUsername and Password are case sensitive")
+    print("Username and Password should be at least 6 characters")
+    time.sleep(1)
+    user_input = input("\nEnter New Username:\n")
+    time.sleep(1)
+    user_password = input("\nEnter New Password: \n")
+    time.sleep(1)
+
+    validate = validate_user_login(user_input, user_password)
+    if validate:
+        login = [user_input, user_password]
+        update_login_data(login)
+        time.sleep(2)
+        os.system('clear')
+        check_user()
+    else:
+        time.sleep(2)
+        add_new_user()
+
+
+def check_user():
+    """
+    Validates the users login information by comparing against
+    data stored in the googlesheet
+    """
+    time.sleep(1)
+    print("\nLOGIN TO PLAY BLACKJACK!")
+    time.sleep(1)
+    username = input("\nUsername: \n")
+    time.sleep(1)
+    password = input("\nPassword: \n")
+
+    logins = get_login_data()
+    check_login = 0
+    for data in logins:
+        if username == data['USERNAME']:
+            if password == data['PASSWORD']:
+                print("\nLog in successful!")
+                time.sleep(2)
+                os.system('clear')
+                # head()
+                current_user['name'] = data['USERNAME']
+                print(f"\nWelcome back {current_user['name']}")
+            else:
+                print("Incorrect password, try again")
+                check_existing_user()
+        else:
+            check_login += 1
+    if check_login == len(logins):
+        print("User does not exist, try again")
+        check_existing_user()
+
+
+check_existing_user()
